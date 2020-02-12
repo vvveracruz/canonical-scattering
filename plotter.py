e4293b1e939d91bb10ba76e14406784df0e19016
Ve <vgonzalezgab@gmail.com>
2020-02-12 13:12:09 +0000

    trying to build the scattered and incident field classes - not working yet

--- plotter.py
+++ plotter.py
@@ -6,9 +6,12 @@ import matplotlib.pyplot as plt
 import scipy.special as sp
 import os
 import time
-#-----------------------------------------------------------------------------
+
+#   NOTE: please try to keep all lines under 70 characters
+
+#--------------------------------------------------------------------
 #                           main class
-#-----------------------------------------------------------------------------
+#--------------------------------------------------------------------
 class Main():
     def __init__(self):
         print("Main")
@@ -16,8 +19,9 @@ class Main():
     def run(self):
         print("doing stuff...")
         graph = Graphics()
-        self.create_hankel_wave(graph)
+        #self.create_hankel_wave(graph)
         #self.create_plane_wave(graph)
+        self.create_incident_field(graph)
 
     def create_hankel_wave(self, graph):
         hankel_wave = ExampleBessel(5)
@@ -27,33 +31,28 @@ class Main():
         plane_wave = PlaneWave('real')
         graph.heat_map(plane_wave)
 
+    def create_incident_field(self, graph):
+        incident_field = IncidentField()
+        graph.heat_map(incident_field)
 
-#-----------------------------------------------------------------------------
-#                           wave super class
-#-----------------------------------------------------------------------------
+
+#--------------------------------------------------------------------
+#                         wave super class
+#--------------------------------------------------------------------
 class Wave():
     def __init__(self, length, delta):
         self.X, self.Y = self.get_xy_series(length, delta)
         self.length = length
         self.name = "Default"
-        #self.Z = self.create_z_series(length, delta)
+        self.wavevector = (1,1) #default
 
     def get_xy_series(self, length, delta):
         x = np.linspace(-length, length, delta)
         y = x
         return np.meshgrid(x, y)
 
-    def get_extent(self):
-        '''
-        Returns the axis labels in the format [xmin, xmax, ymin, ymax].
-        '''
-        l = self.get_length()
-        print(l)
-        return [-l, l, -l, l]
-
-    def create_z_series(self, length, delta):
-        # put code here .... loopy stuff... infinite summmation
-        return None
+    def time_dependence(self, t=1, omega=1):
+        return np.exp(-1j*omega*t)
 
     def set_name(self, new_name):
         self.name = new_name
@@ -70,13 +69,24 @@ class Wave():
     def get_Z(self):
         return self.Z
 
+    def get_wavevector(self):
+        return self.wavevector
+
     def get_length(self):
         return self.length
 
-#-----------------------------------------------------------------------------
+    def get_extent(self):
+        '''
+        Returns the axis labels in the format
+            [xmin, xmax, ymin, ymax]
+        '''
+        return [-self.get_length(), self.get_length(),
+            -self.get_length(), self.get_length()]
+
+#--------------------------------------------------------------------
 #                       concrete wave instantiations
-#-----------------------------------------------------------------------------
-#------------------------------ plane wave -----------------------------------
+#--------------------------------------------------------------------
+#-------------------------- plane wave ------------------------------
 class PlaneWave(Wave):
     def __init__(self, type, length=10 , delta=100):
         super(PlaneWave, self).__init__(length, delta)
@@ -97,26 +107,47 @@ class PlaneWave(Wave):
 
     def phi_imag(self):
         return (self.phi()).imag
+#-------------------------- incident field --------------------------
+class IncidentField(Wave):
+    def __init__(self, length=5, delta=100):
+        super(IncidentField, self).__init__(length, delta)
 
+        self.Z = (self.x_dependence()).real
+        self.set_name("Incident Field")
 
-#--------------------- example bessel function wave ---------------------------
+    def x_dependence(self):
+        k = self.get_wavevector()
+        print(k)
+        return np.exp(-1j*(k[0]*self.get_X + k[1]*self.get_Y))
+#------------------------- scattered field --------------------------
+class ScatteredField(Wave):
+    def __init__(self, length=5, delta=100):
+        super(ScatteredField, self).__init__(length, delta)
+
+    def radial_dependence():
+        return None
+
+    def theta_dependence():
+        return None
+
+#------------------ example bessel function wave --------------------
 class ExampleBessel(Wave):
-    def __init__(self, length=10, delta=100):
+    def __init__(self, length=5, delta=100):
         super(ExampleBessel, self).__init__(length, delta)
         self.Z = self.phi_real()
-
         self.set_name("Example Bessel")
         print("New %s" % self.get_name())
 
     def phi(self):
-        return sp.hankel1(0, self.get_X()*self.get_X()+ self.get_Y()*self.get_Y())
+        return sp.hankel1(0, self.get_X()*self.get_X()
+            + self.get_Y()*self.get_Y())
 
     def phi_real(self):
         return (self.phi()).real
 
-#-----------------------------------------------------------------------------
-#                               graphics class
-#-----------------------------------------------------------------------------
+#−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−-
+#                        graphics class
+#--------------------------------------------------------------------
 class Graphics():
     def __init__(self):
         print("graphics object created")
@@ -139,7 +170,7 @@ class Graphics():
 
 
 
-#-----------------------------------------------------------------------------
+#--------------------------------------------------------------------
 #                               SCRIPT
-#-----------------------------------------------------------------------------
+#--------------------------------------------------------------------
 Main().run()
